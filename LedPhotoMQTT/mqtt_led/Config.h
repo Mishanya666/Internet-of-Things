39b3cb0fdb83911b547da10e9335112fc733b83a
Mishanya666 <149495519+Mishanya666@users.noreply.github.com>
2024-12-21 23:49:17 +0800

    Update Config.h

--- LedPhotoMQTT/mqtt_led/Config.h
+++ LedPhotoMQTT/mqtt_led/Config.h
@@ -1,1 +1,14 @@
+#include "Config.h"
+#include "WIFI.h"
+#include "MQTT.h"
 
+void setup() {
+    Serial.begin(115200);
+    WIFI_init(false);
+    MQTT_init();      
+    pinMode(led, OUTPUT); 
+}
+
+void loop() {
+    mqtt_cli.loop(); 
+}
