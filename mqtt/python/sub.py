1937bba8a2e029e66a29083fa63bba3c18a40465
shokorev25 <63199119+shokorev25@users.noreply.github.com>
2024-12-27 16:28:55 +0800

    добавлен код субскрайбера

---
+++ mqtt/python/sub.py
@@ -0,0 +1,30 @@
+import time
+import paho.mqtt.client as mqtt_client
+import serial
+
+broker = "test.mosquitto.org"
+pub_id = "вставьте айди из паблишера"  
+
+ser = serial.Serial('COM10', 9600)  
+
+client = mqtt_client.Client()
+client.connect(broker)
+client.loop_start()
+
+def on_message(client, userdata, message):
+    data = message.payload.decode('utf-8')  
+    print(f"Received from MQTT: {data}")
+
+    ser.write(f"{data}\n".encode())
+    print(f"Sent to Arduino: {data}")
+
+client.on_message = on_message
+client.subscribe(f"lab/{pub_id}/photo/instant")
+print(f"Subscribed to topic: lab/{pub_id}/photo/instant")
+
+try:
+    while True:
+        time.sleep(1)  
+except KeyboardInterrupt:
+    client.disconnect()
+    client.loop_stop()
