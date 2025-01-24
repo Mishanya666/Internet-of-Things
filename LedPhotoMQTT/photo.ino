76184ef323dcfc44dd7e7285c5f0ade547b63e36
shokorev25 <63199119+shokorev25@users.noreply.github.com>
2025-01-25 06:08:26 +0800

    Добавлен publisher

---
+++ LedPhotoMQTT/photo.ino
@@ -0,0 +1,42 @@
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+
+const char* ssid = "Tenda_05CE60";     // Имя Wi-Fi сети
+const char* password = "82266851";     // Пароль Wi-Fi сети
+const char* mqtt_server = "test.mosquitto.org"; // Адрес MQTT брокера
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+const int photoResistorPin = A0;  // Пин фоторезистора
+const char* topic = "sensor/light"; 
+
+void setup_wifi() {
+  delay(10);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("WiFi connected");
+}
+
+void setup() {
+  Serial.begin(115200);
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+}
+
+void loop() {
+  if (!client.connected()) {
+    while (!client.connect("Publisher")) {
+      delay(500);
+    }
+  }
+
+  int lightLevel = analogRead(photoResistorPin);   // Считывание с датчика
+  client.publish(topic, String(lightLevel).c_str()); // Отправка данных в MQTT
+  Serial.print("Light level: ");
+  Serial.println(lightLevel);
+  delay(1000);  
+}
