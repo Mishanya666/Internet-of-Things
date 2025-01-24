43044963afa565d7620aa998193f87831378cdb8
shokorev25 <63199119+shokorev25@users.noreply.github.com>
2025-01-25 06:08:58 +0800

    Add files via upload
    добавлен субскрайбер

---
+++ LedPhotoMQTT/led.ino
@@ -0,0 +1,57 @@
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+
+const char* ssid = "Tenda_05CE60";     // Имя Wi-Fi сети
+const char* password = "82266851";     // Пароль Wi-Fi сети
+const char* mqtt_server = "test.mosquitto.org"; // Адрес MQTT брокера
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+const int ledPin = 5; // Пин для светодиода (D1)
+const char* topic = "sensor/light";   
+
+void setup_wifi() {
+  delay(10);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("WiFi connected");
+}
+
+void callback(char* receivedTopic, byte* payload, unsigned int length) {
+  payload[length] = '\0'; 
+  String message = String((char*)payload);
+  Serial.print("Message received: ");
+  Serial.println(message);
+
+  int brightness = message.toInt(); 
+  brightness = map(brightness, 0, 1023, 0, 1023); 
+  analogWrite(ledPin, brightness); // Управление светодиодом
+}
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  Serial.begin(115200);
+  setup_wifi();
+
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+
+  if (client.connect("Subscriber")) {
+    client.subscribe(topic);
+    Serial.println("Subscribed to topic: " + String(topic));
+  }
+}
+
+void loop() {
+  if (!client.connected()) {
+    while (!client.connect("Subscriber")) {
+      delay(500);
+    }
+    client.subscribe(topic);
+  }
+  client.loop();
+}
