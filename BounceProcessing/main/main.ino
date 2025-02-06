6c7c9e53e7bf8a63a87722358aa52f7578b2baad
shokorev25 <63199119+shokorev25@users.noreply.github.com>
2025-02-06 13:58:16 +0800

    Update main.ino

--- BounceProcessing/main/main.ino
+++ BounceProcessing/main/main.ino
@@ -2,12 +2,14 @@
 #define BOUNCE_TIME 50
 #define PRESSED HIGH
 
+
 volatile long int press_time = 0;
 volatile bool pressed_candidate = false;
 volatile long int hold_time = 0;
 volatile int press_count = 0;
 volatile bool report_to_user = false;
 bool button_pressed = false;
+
 
 void setup() {
   pinMode(BUTTON_PIN, INPUT);
