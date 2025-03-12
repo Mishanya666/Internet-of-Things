5d606486b2569e3a16dfb43d4bebddacee156a0a
shokorev25 <63199119+shokorev25@users.noreply.github.com>
2025-03-12 09:51:31 +0800

    Update and rename H-bridge/main.ino to H-bridge/hbridge/hbridge.ino

---
+++ H-bridge/hbridge/hbridge.ino
@@ -0,0 +1,34 @@
+
+#define MOTOR_IN1  7  
+#define MOTOR_IN2  8  
+
+void setup() {
+    pinMode(MOTOR_IN1, OUTPUT);
+    pinMode(MOTOR_IN2, OUTPUT);
+}
+
+void controlMotor(int speed, bool direction) {
+    if (direction) {
+        digitalWrite(MOTOR_IN1, HIGH);
+        digitalWrite(MOTOR_IN2, LOW);
+    } else {
+        digitalWrite(MOTOR_IN1, LOW);
+        digitalWrite(MOTOR_IN2, HIGH);
+    }
+    if (speed > 0) {
+        digitalWrite(MOTOR_IN1, HIGH);  
+    } else {
+        digitalWrite(MOTOR_IN1, LOW);  
+    }
+}
+
+void loop() {
+    controlMotor(150, true);  
+    delay(2000);
+    controlMotor(255, true);  
+    delay(2000);
+    controlMotor(150, false); 
+    delay(2000);
+    controlMotor(0, true);    
+    delay(2000);
+}
