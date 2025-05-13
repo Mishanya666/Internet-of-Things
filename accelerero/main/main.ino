e74475afbdec45e43b5764cc53b5daa703ee91ee
shokorev25 <63199119+shokorev25@users.noreply.github.com>
2025-05-13 21:12:02 +0800

    Create main.ino

---
+++ accelerero/main/main.ino
@@ -0,0 +1,44 @@
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_ADXL345_U.h>
+
+Adafruit_ADXL345_Unified accel = Adafruit_ADXL345_Unified(12345);
+
+const int ledPin = 3;
+
+void setup() {
+  Serial.begin(9600);
+
+  if (!accel.begin()) {
+    Serial.println("ADXL345 не найден! Проверь подключение.");
+    while (1); 
+  }
+
+  Serial.println("ADXL345 подключен.");
+  
+  accel.setRange(ADXL345_RANGE_2_G);
+
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  sensors_event_t event;
+  accel.getEvent(&event); 
+
+  Serial.print("X: ");
+  Serial.print(event.acceleration.x);
+  Serial.print(" m/s², Y: ");
+  Serial.print(event.acceleration.y);
+  Serial.print(" m/s², Z: ");
+  Serial.print(event.acceleration.z);
+  Serial.println(" m/s²");
+
+  if (abs(event.acceleration.z) < 0.2) {
+    digitalWrite(ledPin, HIGH); 
+    Serial.println("Невесомость! Светодиод включен.");
+  } else {
+    digitalWrite(ledPin, LOW); 
+  }
+
+  delay(100); 
+}
